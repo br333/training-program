00f33d9d4cf89465c9d5b544c7b5855701860de8
johndmguerrero <guerrero.johndm@gmail.com>
2020-06-22 16:52:07 +0800

    previous commit is wrong file, pushing for real file

--- John_Guerrero/exe2.rb
+++ John_Guerrero/exe2.rb
@@ -1,95 +1,73 @@
 
-
-
-
 class Domain
-  attr_accessor :name, :registration_date, :expiration_date, :user
-  REGISTERED_DOMAINS = []
 
+  attr_accessor :name
 
-  def initialize(name, registration_date, expiration_date, user)
-    self.name              = name
-    self.registration_date = registration_date
-    self.expiration_date   = expiration_date
-    self.user              = user
+  LIST_DOMAINS = ["google.com.ph","google.ph","watercup.net.ph","watr.com.ph","w
+    ter.ph","water.com.ph","googlemask.ph","go0gl3mask.ph","goglemaske.com.ph","wheninr
+    me.com.ph","wheninrone.com.ph","iotechnologies.com.ph","itlog.ph","i0tehnologies.co
+    .ph","waiter.com.ph","lenvovo.org.ph","orgnanization.org.ph","organization.org.ph"]
+
+  def initialize(name)
+    self.name = name
+
   end
 
-  def self.is_available?(name)
-    !REGISTERED_DOMAINS.map(&:name).include?(name)
-  end
+  # def self.is_available?(name)
+  #   !LIST_DOMAINS.map(&:name).include?(name)
+  # end
+  
+  # def register
 
+  #   if self.class.is_available?(name)
+  #     LIST_DOMAINS.push(self)
+  #   else
+  #     LIST_DOMAINS.each do |domain_object|
+  #       break domain_object.expiration_date if domain_object.name == name
+  #     end
+  #   end
+  # end
 
-  def register
-    if self.class.is_available?(name)
-      REGISTERED_DOMAINS.push(self)
+  def search
+
+    if LIST_DOMAINS.include?(self.name)
+      'is available.'
     else
-      # 'Domain is not available'
-      REGISTERED_DOMAINS.map{|registered_user| registered_user}
+      'is not unavailable.'
     end
   end
 
-  def registration_date_and_expiration_date
-    hash = {}
-    hash['registration_date'] = registration_date
-    hash['expiration_date']   = expiration_date
-    hash
-  end
-
-end
-
-
-@registered_users = []
-
-
-class User
-  attr_accessor :first_name, :last_name, :id, :role
-
-  ARRAY_OF_ROLES = [
-    'SUPER ADMIN',
-    'CUSTOMER SUPPORT',
-    'ADMIN'
-  ]
-
-  def initialize(first_name, last_name, id)
-    self.first_name = first_name
-    self.last_name  = last_name
-    self.id         = id
-    self.role       = nil
-  end
-
-  def set_random_role!
-    self.role = ARRAY_OF_ROLES.sample
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
 end
 
 
 
-def register_user(user)
-  @registered_users.push(user)
+
+class Val
+
+  attr_accessor :text
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def validation
+
+
+
+    self.gsub(/[^0-9A-Za-z]/,'')
+
+    if self.length < 3 
+      'domain too short'
+    elsif self.length > 63
+      'domain length too much'
+    end
+
+  end
+  
 end
 
+puts "Enter a domain"
 
+name = gets.chomp
 
-# COMMANDS
-
-# user_1 = User.new('first_name_1', 'last_name_1', 1)
-# user_2 = User.new('first_name_2', 'last_name_2', 2)
-
-# user_1.set_random_role!
-# user_2.set_random_role!
-
-# register_user(user_1)
-# register_user(user_2)
-
-# @registered_users
-
-
-# @registered_users.map{|registered_user| registered_user}
-
-
-# @registered_users.map(&:first_name)
+Val.validation
