f0c802f6234d866bf85c7744efbc607df8d6428f
Itchan <christianbaman4@gmail.com>
2020-06-22 17:22:48 +0800

    registration_app

--- christian_carijutan/registration_app.rb
+++ christian_carijutan/registration_app.rb
@@ -1,4 +1,5 @@
 class Register
+  
     DOMAINS = [
         {  "name" => "facelinkedin.com.ph", "registration_date" => Time.new(2021 , 1 , 12), "expiration_date" => Time.new(2022, 1, 12)}, 
         {  "name" => "pinterest.com.ph", "registration_date" =>  Time.new(2021 , 1 , 12), "expiration_date" =>  Time.new(2022, 1, 12)} 
@@ -12,6 +13,17 @@ class Register
         self.expiration_date   = expiration_date
       end
 
+      def self.is_available?(name)
+        !REGISTERED_DOMAINS.map(&:name).include?(name)
+      end
+      
+      def renew
+        if self.class.is_available?(name)
+          registration.register
+        else
+          registration.search
+        end
+      end
      
       def register
         print "Please enter your choice: \n"
@@ -35,6 +47,32 @@ class Register
           expiration_date + 36.months
           DOMAINS.push(self)
         end
-
       end
+
+  def search
+      print "Please enter your choice: \n"
+      print "Press 1 to search all \n"
+      print "Press 2 to search by domain \n"
+      choice = gets
+      
+      integer = choice.to_i
+      search = Search::DOMAINS
+
+      if integer == 1
+          puts search
+      end
+      if integer == 2        
+          print "Please enter your domain: \n"
+          domain = gets
+          
+          search = 
+
+          puts DOMAINS.find{[x] x['name'] == }
+      end
+    end
+
+  registration = Register.new
+  registration.search
+  registration.register
+
 end
