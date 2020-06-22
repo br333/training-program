795e230fa509fe9d698fad2c8cac1ce32bfa9fe5
Janmar Roque Jr <janmarroquejr@gmail.com>
2020-06-22 17:51:47 +0800

    test 3,4 unfinished

--- janmar_roque_jr/domain_availability.rb
+++ janmar_roque_jr/domain_availability.rb
@@ -1,12 +1,12 @@
 
-
+REGISTERED_DOMAINS = []
 
 
 
 class Domain
     attr_accessor :name, :reg_date, :exp_date
 
-    REGISTERED_DOMAINS = []
+    
 
     def initialize(name, reg_date, exp_date)
         self.name = name
@@ -19,30 +19,45 @@ class Domain
     # end
 
     def register
-        if !REGISTERED_DOMAINS.map(&:name).include?(name)
             REGISTERED_DOMAINS.push(self)
-        else
-            REGISTERED_DOMAINS.each do |domain_object|
-                break domain_object.expiration_date if domain_object.name == name
-            end
-        end
     end
-
-
-    def search(name)
-        if !REGISTERED_DOMAINS.map(&:name).include?(name)
-            puts "That domain name is available"
-        else
-            puts "That domain name is unavailable"
-        end
-    end 
-
 end
+
+is_available = false
+def search(search)
+    if !REGISTERED_DOMAINS.map(&:name).include?(search)
+        puts "That domain name is available"
+    else
+        puts "That domain name is unavailable"
+        is_available = true
+        reg
+    end
+end 
+
+def reg
+    puts "Enter registration period: "
+    puts "a. 3 months"
+    puts "b. 1 year"
+    puts "c. 2 years"
+    puts "d. 5 years"
+    puts "e. 10 years"
+    period = gets.chomp()
+    reg_date = DateTime.today
+
+    if period == 'a'
+        domain = Domain.new(name, reg_date, (reg_date + 3.months))
+    end
+end
+
 
 domain1 = Domain.new('google.com.ph', Time.new(2020,1,1), Time.new(2021,1,1))
 domain1.register
 # domain2 = ('google.ph', 2020,1,1, 2021,1,1)
+domain2 = Domain.new('google.ph', Time.new(2020,1,1), Time.new(2021,1,1))
+domain2.register
 # domain3 = ('watercup.net.ph', 2020,1,1, 2021,1,1)
+domain1 = Domain.new('watercup.net.ph', Time.new(2020,1,1), Time.new(2021,1,1))
+domain1.register
 # domain4 = ('watr.com.ph', 2020,1,1, 2021,1,1)
 # domain5 = ('water.ph', 2020,1,1, 2021,1,1)
 # domain6 = ('water.com.ph', 2020,1,1, 2021,1,1)
@@ -60,4 +75,16 @@ domain1.register
 # domain18 = ('organization.org.ph', 2020,1,1, 2021,1,1)
 puts "Please input a domain name: "
 name = gets.chomp()
-name.search
+
+if name.length < 3 and name.include?('-') and name.length > 63
+    puts "Domain name invalid."
+else
+    search(name)
+end
+
+
+# puts "Do you want to register? Y/N"
+# is_reg = gets.chomp()
+
+# puts "Please input registration length(year/s): "
+# years = gets.chomp()
